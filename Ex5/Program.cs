0085037e9f5f1a970cc1075ff6ba2673d0605cae
diaz-L <lediar.a.diaz@gmail.com>
2021-06-11 09:03:38 -0400

    ex5 completed: create a function that finds the index of an item in a given array

---
+++ Ex5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+ * Create a function that finds the index of a given item
+ * If the item is not present, return -1
+ */
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+				var result1 = IndexOf(new int[] {1,5,3}, 5);
+				var result2 = IndexOf(new int[] {9,8,3}, 3);
+				var result3 = IndexOf(new int[] {1,2,3}, 4);
+
+				Console.WriteLine($"{result1}\n{result2}\n{result3}");
+        }
+
+			static int IndexOf(int[] list, int target)
+			{
+				for (var i = 0; i < list.Length; i++)
+					if (list[i] == target) return i;
+
+				return -1;
+			}
+    }
+}
