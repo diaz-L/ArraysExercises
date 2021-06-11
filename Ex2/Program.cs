12c177647ab677d8537b0f9fec515153eb4be324
diaz-L <lediar.a.diaz@gmail.com>
2021-06-10 20:59:16 -0400

    ex2 completed: create method that returns the difference between the biggest and smallest number in given array

---
+++ Ex2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+/*
+ * Give an array of integers, return the difference between the largest and smallest integers of the array
+ */
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+				int result1 = Diff(new int[] {10, 15, 20, 2, 10, 6});	//18
+				int result2 = Diff(new int[] {-3, 4, -9, -1, -2, 15});	//24
+				int result3 = Diff(new int[] {4, 17, 12, 2, 10, 2});	//15
+
+				Console.WriteLine($"{result1}\n{result2}\n{result3}");
+        }
+
+			static int Diff(int[] list)
+			{
+				int max = Max(list);
+				int min = Min(list);
+				return max - min;
+			}
+
+			static int Max(int[] numbers)
+			{
+				int max = Int32.MinValue;
+				foreach (var n in numbers)
+					if (n > max) max = n;
+
+				return max;
+			}
+			
+			static int Min(int[] numbers)
+			{
+				int min = Int32.MaxValue;
+				foreach (var n in numbers)
+					if (n < min) min = n;
+
+				return min;
+			}
+    }
+}
