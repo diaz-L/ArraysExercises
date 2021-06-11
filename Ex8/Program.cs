c0e2b6883e5871a2dd527b447d0d817cbfafcda8
diaz-L <lediar.a.diaz@gmail.com>
2021-06-11 11:01:54 -0400

    -modified sln file to include Ex8 project ex8 completed: create a function that finds the largest number in a given array

---
+++ Ex8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+ * Create a function that takes an array of numbers, and returns the largest number in the array.
+ */
+namespace Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindLargestNumber(new int[] {4, 5, 1, 3}));
+				Console.WriteLine(FindLargestNumber(new int[] {300, 200, 600, 150}));
+				Console.WriteLine(FindLargestNumber(new int[] {1_000, 1_001, 857, 1}));
+        }
+
+			static int FindLargestNumber(int[] numbers)
+			{
+				int max = Int32.MinValue;
+
+				foreach (var n in numbers)
+					if (n > max) max = n;
+
+				return max;
+			}
+    }
+}
