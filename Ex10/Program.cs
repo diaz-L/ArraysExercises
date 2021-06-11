2878e3a52c8da600f114fadcc4e0012c0c88e1d3
diaz-L <lediar.a.diaz@gmail.com>
2021-06-11 11:55:13 -0400

    -modified solution file to include project ex10 -modified gitignore -ex10 completed: create a functio that takes in an array of numbers and returns the sum of its cubes

---
+++ Ex10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+ * Create a function that takes in an array of numbers and returns the sum of its cubes.
+ */
+namespace Ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+				Console.WriteLine(SumOfCubes(new int[] {1, 5, 9}));	
+				Console.WriteLine(SumOfCubes(new int[] {3, 4, 5}));	
+				Console.WriteLine(SumOfCubes(new int[] {2}));	
+        }
+
+		 static int SumOfCubes(int[] numbers)
+		 {
+				int sum = 0;
+
+				foreach (var n in numbers)
+					sum += (int) Math.Pow(n, 3);
+
+				return sum;
+		 }
+    }
+}
