a5c2cd634135706ccaba19ae151d79b99855e3a8
diaz-L <lediar.a.diaz@gmail.com>
2021-06-11 15:09:04 -0400

    -updated sln to include project ex11 completed Ex11: write a method to check if an array contains a particular number

---
+++ Ex11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+ * Write a method to check if an array contains a particular number.
+ */
+namespace Ex11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+				var result1 = Has(new int[] {1,2,3,4,5}, 3);
+				var result2 = Has(new int[] {1,1,2,1,1}, 3);
+				var result3 = Has(new int[] {5,5,5,6}, 5);
+				var result4 = Has(new int[0], 5);
+
+				Console.WriteLine($"{result1}\n{result2}\n{result3}\n{result4}");
+        }
+
+			static bool Has(int[] numbers, int match)
+			{
+				foreach (var n in numbers)
+					if (n == match) return true;
+
+				return false;
+			}
+    }
+}
