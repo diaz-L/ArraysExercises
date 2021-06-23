a0ccdc4e5c91dfdbd576300a08d67459c3291f4a
diaz-L <lediar.a.diaz@gmail.com>
2021-06-22 22:47:09 -0400

    --completed ex14: create a function that takes a number and returns the name of its corresponding month --edited solution file to include ex14

---
+++ Ex14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+ * Create a function that takes a number (1 to 12) and returns its correspoding month name as a string.
+ */
+namespace Ex14
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(MonthName(3));
+      Console.WriteLine(MonthName(12));
+      Console.WriteLine(MonthName(6));
+    }
+
+    static string MonthName(int n)
+    {
+      string[] months = {
+                "January", "February", "March", "April",
+                "May", "June", "July", "August", "September",
+                "October", "November", "December"
+            };
+
+      return n < 1 || n > 12 ? "Unknown" : months[n - 1];
+    }
+  }
+}
